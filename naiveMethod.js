a9c4c28f70015e86553e07657e27187902556ba5
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-01-18 13:55:51 +0000

    Extraction Emails naive Method completed

--- naiveMethod.js
+++ naiveMethod.js
@@ -4,11 +4,9 @@ const contents = fs.readFileSync("test.txt");
 const stringData = contents.toString();
 // find pieces of string in email format and add to a counter. 
 // exclude strings that continue after @softwire.com (e.g. rosemaria.oniskey@softwire.comet.net) (must have white space after) -- DONE
-// find a way to exclude string that only contain @softwire.com and nothing before (must not have whitespace before) -- IN PROGRESS
 // final answer is 261
-// what if @sofwire.com is at the end of line?
- 
+// what if @sofwire.com is at the end of line? -- DONE
 for (var i = 0; i < stringData.length; i++) {
-    if ((stringData.substring(i, i+ 14) === '@softwire.com ')) { emailcount++ }
+    if ((stringData.substring(i, i + 14) === '@softwire.com ') || (stringData.substring(i, i + 15) === '@softwire.com' + "\r\n")) { emailcount++ }
 }
 console.log(emailcount);
